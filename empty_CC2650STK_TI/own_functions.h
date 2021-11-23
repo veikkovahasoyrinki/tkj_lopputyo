aac976535b51ace9115d29db8708442fc598a96a
veikkovahasoyrinki <71126486+veikkovahasoyrinki@users.noreply.github.com>
2021-11-23 15:45:18 +0200

    Update own_functions.h

--- empty_CC2650STK_TI/own_functions.h
+++ empty_CC2650STK_TI/own_functions.h
@@ -8,9 +8,9 @@
 #ifndef OWN_FUNCTIONS_H_
 #define OWN_FUNCTIONS_H_
 
-int liiku_check(float *ax, float *ay);
+int liiku_check(float *ax, float *ay, float *az);
 int data_activate(float *valoisuusarvo, float *ax, float *ay);
-void syo_check(float *az, int *laskuri_az, int *flag1);
-int leiki_check(float *gx);
+void syo_check(float *az,float *ay, float *ax, int *laskuri_az, int *flag1);
+int leiki_check(float *gx, float *gy, float *gz);
 
 #endif /* OWN_FUNCTIONS_H_ */
