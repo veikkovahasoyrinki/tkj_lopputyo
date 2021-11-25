5779e28810417ac53b39096e99443850652f5c74
veikkovahasoyrinki <71126486+veikkovahasoyrinki@users.noreply.github.com>
2021-11-25 10:18:12 +0200

    Update own_functions.h

--- finaali/own_functions.h
+++ finaali/own_functions.h
@@ -11,6 +11,6 @@
 int liiku_check(float *ax, float *ay, float *az);
 int data_activate(float *valoisuusarvo, float *ax, float *ay);
 void syo_check(float *az,float *ay, float *ax, int *laskuri_az, int *flag1);
-int leiki_check(float *gx, float *gy, float *gz);
+int leiki_check(float *gx);
 
 #endif /* OWN_FUNCTIONS_H_ */
